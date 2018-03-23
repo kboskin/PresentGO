691588c499e6fadc4819f3e570d32c35c371488c
kostyaBoss <kostyasharp787898@gmail.com>
2018-03-23 23:58:52 +0200

    - Fixed bug with admob in last fragment

--- app/src/main/java/com/blacksmithdreamapps/presentgo/fragments/AfraidToForgetInputFragment.kt
+++ app/src/main/java/com/blacksmithdreamapps/presentgo/fragments/AfraidToForgetInputFragment.kt
@@ -12,6 +12,10 @@ import com.blacksmithdreamapps.presentgo.R
 import com.blacksmithdreamapps.presentgo.activities.GiftActivity
 import com.blacksmithdreamapps.presentgo.events.DataPassingEvent
 import com.blacksmithdreamapps.presentgo.events.PagerPasserEvent
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.fragment_fragment_afraid_to_forget_input.*
 import kotlinx.android.synthetic.main.fragment_fragment_afraid_to_forget_input.view.*
 import org.greenrobot.eventbus.EventBus
@@ -29,6 +33,8 @@ class AfraidToForgetInputFragment : Fragment() {
 
     // handling passing here viewPager via eventbus
     var bus = EventBus.getDefault()
+
+    private lateinit var mInterstitialAd: InterstitialAd
 
     @Subscribe
     public fun setUpViewPager(event: PagerPasserEvent) {
@@ -51,11 +57,36 @@ class AfraidToForgetInputFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        // admob
+        MobileAds.initialize(context, "ca-app-pub-2631718830975455~9288409657")
+
+        mInterstitialAd = InterstitialAd(context)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.adListener = object : AdListener() {
+
+            override fun onAdClosed() {
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(AdRequest.Builder().addTestDevice("4AB458BDDA5C649998AB1AA81B0EEE8E").build())
+                startActivity(Intent(context, GiftActivity::class.java))
+            }
+
+            override fun onAdFailedToLoad(p0: Int) {
+                startActivity(Intent(context, GiftActivity::class.java))
+            }
+        }
+
+        mInterstitialAd.loadAd(AdRequest.Builder().addTestDevice("4AB458BDDA5C649998AB1AA81B0EEE8E").build())
+
+
         // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_fragment_afraid_to_forget_input, container, false)
         // skip calendar integration and show fragment
         view.show_me_gift.setOnClickListener {
-            startActivity(Intent(context, GiftActivity::class.java))
+            if (mInterstitialAd.isLoaded) {
+                mInterstitialAd.show()
+            } else {
+            }
         }
         view.button_done_finally.setOnClickListener {
 
